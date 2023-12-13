cb94ad8995f7ecaa1ad5ddb6bdc4f31fe4132fb5
Eddas J. Bertrand <eddas_email@yahoo.com>
2023-12-13 20:15:40 -0300

    Added a hero editor, as well as displayed the list of heroes with the details of the selected hero.

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [CommonModule, FormsModule, NgFor, NgIf],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
+}
