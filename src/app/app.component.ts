de90b47c3c652a1e142c81aa9630daa1f317f983
Eddas J. Bertrand <eddas_email@yahoo.com>
2023-12-22 16:33:46 -0600

    Added messages display service, and heroes retrieval service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 import { HeroesComponent } from './heroes/heroes.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet,
-            HeroesComponent],
+            HeroesComponent, MessagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgFor } from '@angular/common';
 
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
+import { MessagesService } from '../messages.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,11 +13,22 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
-export class HeroesComponent {
-  heroes = HEROES;
+export class HeroesComponent implements OnInit{
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+
+  constructor(private heroService: HeroService, private messagesService: MessagesService){}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
+  }
 
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
+    this.messagesService.add(`Heroes Component: Selected hero id=${hero.id}`);
   }
 }
